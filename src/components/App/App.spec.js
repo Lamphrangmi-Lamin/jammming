4963f996a54a1520ee36c9e85671544dce0557e9
Lamphrangmi-Lamin <lamphrangmilamin46169@gmail.com>
2024-02-15 09:10:51 +0530

    configuring jest and react testing library

---
+++ src/components/App/App.spec.js
@@ -0,0 +1,30 @@
+function plusTwo(x) {
+    return x + 2;
+};
+
+console.log(plusTwo(8));
+
+ describe('plusTwo', () => {
+    it('add 2 to any value of x', () => {
+        expect(plusTwo(8)).toBe(10);
+    })
+    it('add 2 to any value of x', () => {
+        expect(plusTwo(10)).toBe(12);
+    })
+ })
+ describe('plusTwo', () => {
+    it('add 2 to any value of x', () => {
+        expect(plusTwo(8)).toBe(10);
+    })
+    it('add 2 to any value of x', () => {
+        expect(plusTwo(10)).toBe(12);
+    })
+ })
+ describe('plusTwo', () => {
+    it('add 2 to any value of x', () => {
+        expect(plusTwo(8)).toBe(10);
+    })
+    it('add 2 to any value of x', () => {
+        expect(plusTwo(10)).toBe(12);
+    })
+ })
